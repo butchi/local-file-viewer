acc7277ab47426f7ca0c6f688c3017b45a38aaec
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-01-12 05:25:52 +0900

    lsのAPIからencodeURIして返す

--- api/routes/api/ls.js
+++ api/routes/api/ls.js
@@ -5,23 +5,18 @@ const fs = require('fs');
 
 const listFiles = dirPath => {
   const fileArr = [];
-  const pathArr = fs.readdirSync(dirPath);
+  const pathArr = fs.readdirSync(decodeURIComponent(dirPath));
 
   for (let name of pathArr) {
     try {
       const path = `${dirPath}/${name}`;
       const stat = fs.statSync(path);
 
-      switch (true) {
-        case stat.isFile():
-          fileArr.push(name);
-          break;
-
-        case stat.isDirectory():
-          fileArr.push(name + '/');
-          break;
-
-        default:
+      if (stat == null) {
+      } else if (stat.isFile()) {
+        fileArr.push(encodeURIComponent(name));
+      } else if (stat.isDirectory()) {
+        fileArr.push(encodeURIComponent(name + '/'));
       }
     } catch (err) {
       console.error('error:', err.message);
