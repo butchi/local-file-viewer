cb60b7c39662c14a42e3a5be3f15d87e48be12f6
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-01-19 17:22:25 +0900

    ルートパスの後のスラッシュは不要だった

--- pages/index.vue
+++ pages/index.vue
@@ -158,7 +158,7 @@ export default {
       title: "Local file viewer",
       itemArr: [
         {
-          id: rootPath + "/",
+          id: rootPath,
           name: "~/",
           children: [],
         },
